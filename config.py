d15f08d80091774f38aa72a40e113d317c9782ff
balaji821 <75879172+balaji821@users.noreply.github.com>
2025-04-30 16:33:14 +0530

    Added email

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ logger.setLevel(logging.INFO)
 # API keys and other configurations
 GOOGLE_GENAI_API_KEY = "AIzaSyDjcafJ0J82XLF2jcki5Klott4haQzyRx8"
 EMAIL_SENDER = 'balaji2000001@gmail.com'
-EMAIL_RECIPIENTS = ['balajis8201@gmail.com','balaji.srinis@zohocorp.com']
+EMAIL_RECIPIENTS = ['balajis8201@gmail.com','balaji.srinis@zohocorp.com','nandhini.sivasankar@zohocorp.com']
 EMAIL_SMTP_SERVER = 'smtp.gmail.com'
 EMAIL_SMTP_PORT = 587
 EMAIL_SMTP_USERNAME = 'balaji2000001'
